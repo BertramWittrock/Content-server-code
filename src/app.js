0eaa7f592bf553abcf9d4e899dc2f35a5c9260e1
Bertram Wittrock <bertramvw@yahoo.dk>
2023-11-26 15:51:08 +0100

    auth middleware hala

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const notesRoutes = require('./routes/notesRoutes');
-const commentsRoutes = require('./routes/commentsRoutes');
+const express = require("express");
+const notesRoutes = require("./routes/notesRoutes");
+const commentsRoutes = require("./routes/commentsRoutes");
+const auth = require("./middleware/auth");
 const app = express();
 app.use(express.json());
-app.use(express.static('src/public'));
+app.use(express.static("src/public"));
 
-app.use('/sticky-notes', notesRoutes);
-app.use('/comments', commentsRoutes);
+app.use("/sticky-notes", auth, notesRoutes);
+app.use("/comments", auth, commentsRoutes);
 
-module.exports = app
+module.exports = app;
 
 // const express = require('express');
 // const path = require('path');
