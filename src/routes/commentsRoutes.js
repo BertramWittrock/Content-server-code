39642b864964ee1d1fed73021b7547f0a38e115a
Bertram Wittrock <bertramvw@yahoo.dk>
2023-11-26 14:49:21 +0100

    Update commentsRoutes.js

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database/db').connectToDatabase; // Antager at du har en funktion til at forbinde til databasen i db.js
-const { encrypt, decrypt } = require('../utils/encryption'); // Antager, at du har separeret krypteringslogikken til en fil 'crypto.js'
+const commentsController = require('../database/comments');
+const { encrypt, decrypt } = require('../utils/encryption');
 
 // POST Endpoint til at oprette en kommentar
 router.post('/', async (req, res) => {
   const { noteId, username, comment } = req.body;
   const encryptedComment = encrypt(comment);
   const timestamp = new Date().toISOString();
-  const database = await db();
   
   try {
-    await database.run('INSERT INTO comments (note_id, username, comment, timestamp) VALUES (?, ?, ?, ?)', [noteId, username, encryptedComment, timestamp]);
-    res.status(201).send({ message: 'Kommentar oprettet!' });
+    const result = await commentsController.addComment(noteId, username, encryptedComment, timestamp);
+    
+    if (result.success) {
+      res.status(201).send({ message: 'Kommentar oprettet!' });
+    } else {
+      throw new Error(result.error);
+    }
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
@@ -21,10 +25,9 @@ router.post('/', async (req, res) => {
 // GET Endpoint til at hente kommentarer for en sticky note
 router.get('/:noteId', async (req, res) => {
   const noteId = req.params.noteId;
-  const database = await db();
   
   try {
-    const comments = await database.all('SELECT * FROM comments WHERE note_id = ?', [noteId]);
+    const comments = await commentsController.getComments(noteId);
     const decryptedComments = comments.map(comment => ({
       ...comment,
       comment: decrypt(comment.comment)
