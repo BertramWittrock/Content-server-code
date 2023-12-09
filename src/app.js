3f0a3b07d01379e7fd2d7bb594468491fba73061
Bertram Wittrock <bertramvw@yahoo.dk>
2023-12-09 16:26:17 +0100

    helmet

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ const notesRoutes = require('./routes/notesRoutes');
 const commentsRoutes = require('./routes/commentsRoutes');
 const app = express();
 const auth = require('./middleware/auth')
+const helmet = require('helmet');
 app.use(express.json());
 app.use(express.static('public'));
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      connectSrc: ["'self'", "wss://eu.joeandthejuice.digital"]
-    }
+app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'", "wss://eu.joeandthejuice.digital"],
+        // ... other directives as needed ...
+      },
+    },
   }));
 app.use('/sticky-notes', notesRoutes);
 app.use('/comments', commentsRoutes);
