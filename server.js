d8f38fd24c64e23aaa7af2c894286493f5426531
Bertram Wittrock <bertramvw@yahoo.dk>
2023-12-20 16:02:59 +0100

    d

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const http = require('http');
-const app = require('./src/app.js');
-const { createSocketServer } = require('./src/sockets/socketHandlers');
+const http = require("http");
+const app = require("./src/app.js");
+const { createSocketServer } = require("./src/sockets/socketHandlers");
 
 const server = http.createServer(app);
 createSocketServer(server);
 
-const PORTS = [3000,3001,3002,3003,3004,3005,3006,3007,3008,3009];
-PORTS.forEach(port => {
-  server.listen(port, () => {
-    console.log(`Serveren er startet på port ${port}`);
-  });
-})
-
+const port = 3000
+server.listen(port, () => {
+  console.log(`Serveren er startet på port ${port}`);
+});
